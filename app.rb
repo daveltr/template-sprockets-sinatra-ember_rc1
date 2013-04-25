ed59d6cb9d4f1011617891343cdae88f5612eff9
Nrion Fx <nrion.fx@gmail.com>
2013-04-25 16:07:36 -0400

    auth 401

--- app.rb
+++ app.rb
@@ -1,13 +1,29 @@
+
 class App < Sinatra::Base
 
   set :views, settings.root + '/app/views'
 
   get '/' do
+    error 401 unless valid_key?('123')
     erb :main
   end
   get '/test' do 
     erb :test
   end
   
+  post 'token' do
+
+  end
+
+  def valid_key?(key)
+    return true if key = '123'
+    return false
+  end
+
+  error 401 do 
+    'Not Authorized'
+  end
+
+
 end
 
