c486b4a09146b32e7d15de29643c15bb399e4693
Nrion Fx <nrion.fx@gmail.com>
2013-05-13 14:32:34 -0400

    cleaned

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ gem 'sinatra', require: 'sinatra/base'
 gem 'sprockets', '~> 2.5.0'
 gem 'shotgun'
 gem 'coffee-script'
-# gem 'sinatra-ember', :git => 'https://github.com/gamepoet/sinatra-ember', :require => 'sinatra/ember'
 
 gem 'handlebars_assets'
 gem 'sass'
+
+gem 'thin'
--- app.rb
+++ app.rb
@@ -21,39 +21,6 @@ class App < Sinatra::Base
     erb :test
   end
 
-  #create session
-  post '/api/session/:token' do 
-    # token is the user access token
-    # store this session somewhere??
-    # lookup_by_api_key
-    {:token=>params[:token]}.to_json
-  end
-  
-  #destroy the session
-  delete '/api/session/' do 
-    # remove the session
-    {:message=>'session closed'}.to_json
-  end
-  
-  #get information about current session
-  get '/api/session' do 
-    dat = {
-      :username=>'Dave',
-      :token=>'ABC1234DEF567'
-    }
-    return dat.to_json
-  end
-
-
-  get '/api/navbar' do 
-    token = "ABC1234DEF567"
-    error 401 unless valid_key?(token) 
-    df = DataFixtures.new
-    {:nav_items=>df.blocks}.to_json
-  end
-
-
-
   
 end
 
